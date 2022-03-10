3633d08f796068d0c1bc9aa5de359da81a649d95
Thomas Castaldi <tomas2mars@hotmail.fr>
2022-03-10 14:33:46 +0100

    Modif PHP contact page

--- contact.php
+++ contact.php
@@ -10,11 +10,13 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
     $message = addslashes($_POST['message']);
 
 $to = "tomas2mars@hotmail.fr";
-$subject = "Demande informations Mobil*home St Tropez";
+$subject = "Demande informations Mobil-home St Tropez";
 $body = "Nom : ".$firstname. $lastname. "\r\n".
-        "Email : ".$email."\r\n".
+        "Email : ".$email. "\r\n".
+        "Date d'arrivée : ".$checkin. "\r\n".
+        "Date de départ : ".$checkout. "\r\n".
         "Message : ".$message;
-$header = "From:mobilhome@lemobilhomedesttrop.go.yo.fr"."\r\n".
+$header = "From:mobil-home@lemobilhomedesttrop.go.yo.fr"."\r\n".
             "Reply-To:".$email."\e\n".
             "X=Mailer:PHP/".phpversion();
 
@@ -23,9 +25,8 @@ if(mail($to, $subject, $body, $header)){
     echo("Votre demande a bien été envoyée avec succès !");
 
 }else{
-    echo("Malheureusement, votre demande n'a pas pu être envoyée. Merci de réessayer")
+    echo("Malheureusement, votre demande n'a pas pu être envoyée. Merci de réessayer");
 }
 }
-
 
 ?>
